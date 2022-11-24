cec15d3b169136982553fa8ee7de2d9e091c23ce
Shafa Aleyda Tsabitah <91953656+flynnrider951230@users.noreply.github.com>
2022-11-24 14:15:01 +0700

    Update main.dart

--- counter_7/lib/main.dart
+++ counter_7/lib/main.dart
@@ -18,6 +18,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
+       routes:{
+        '/detail': (context) => const MyWatchlistDetailPage(), 
+      },
     );
   }
 }
